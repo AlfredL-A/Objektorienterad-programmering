81949fc1efc17c15742a2977c0f8cf075ad16b9a
AlfredL-A <32062650+AlfredL-A@users.noreply.github.com>
2017-09-18 20:53:23 +0200

    Update Uppgift12b.java

--- laboration1/Uppgift12b.java
+++ laboration1/Uppgift12b.java
@@ -4,7 +4,11 @@ public class Uppgift12b {
         // variable setup
         double sum = 0, n = 1, term = 0;
         int pm = 1;
-
+        
+        // jag satte term = 1 så att jag direkt kunde gå in i en while 
+        // samt skrev pm / n, men är lite osäker om mitt blir otydligt
+        // märkte även att man kan skriva pm *= -1
+        
         // calculate the sum
         do {
             term = pm * (1 / n);
